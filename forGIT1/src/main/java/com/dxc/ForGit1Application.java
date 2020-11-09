6a3f7a7a1f389ddcba66acb0705d2229bceeb0c0
palpurbita <purbita.july17.1997@gmail.com>
2020-11-09 20:49:58 +0530

    First commit

---
+++ forGIT1/src/main/java/com/dxc/ForGit1Application.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForGit1Application.class, args);
+	}
+
+}
---
+++ forGIT1/src/main/java/com/dxc/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.dxc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		
+		return "Hello World";
+	}
+
+}
